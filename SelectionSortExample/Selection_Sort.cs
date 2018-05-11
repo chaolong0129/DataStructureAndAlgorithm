a5e7c76c633a11d3decb3e4c7bc6140766a5897c
Robert Chen <robert.chen@fitivision.com>
2018-05-11 17:57:20 +0800

    To Format the Selection Sort.

--- SelectionSortExample/Selection_Sort.cs
+++ SelectionSortExample/Selection_Sort.cs
@@ -1,26 +1,22 @@
 using System;
 
-namespace SelectionSortExample
-{
-    public class Selection_Sort
-    {
-        public static void Sort<T>(T[] array) where T : IComparable<T>
-        {
+namespace SelectionSortExample {
+    public class Selection_Sort {
+        public static void Sort<T> (T[] array) where T : IComparable<T> {
             for (int i = 0; i < array.Length; i++) {
                 int minIndex = i;
                 T minValue = array[i];
-                for (int j = i+1; j <= array.Length - 1; j++) {
-                    if (array[j].CompareTo(minValue) < 0) {
+                for (int j = i + 1; j <= array.Length - 1; j++) {
+                    if (array[j].CompareTo (minValue) < 0) {
                         minIndex = j;
                         minValue = array[j];
                     }
                 }
-                SWAP(array, i, minIndex);
+                SWAP (array, i, minIndex);
             }
         }
 
-        private static void SWAP<T>(T[] array, int first, int second)
-        {
+        private static void SWAP<T> (T[] array, int first, int second) {
             T tmp = array[first];
             array[first] = array[second];
             array[second] = tmp;
