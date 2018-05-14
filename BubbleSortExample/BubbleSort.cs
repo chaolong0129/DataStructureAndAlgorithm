0046068c094bbf2157b704debdebbbd19da8c6a4
Robert Chen <robert.chen@fitivision.com>
2018-05-14 15:17:12 +0800

    [Algorithm] Update Bubble Sort.

--- BubbleSortExample/BubbleSort.cs
+++ BubbleSortExample/BubbleSort.cs
@@ -1,20 +1,16 @@
-namespace BubbleSortExample
-{
+namespace BubbleSortExample {
     using System;
-    public class BubbleSort
-    {
-        public static void Sort<T>(T[] array) where T : IComparable<T>
-        {
+    public class BubbleSort {
+        public static void Sort<T> (T[] array) where T : IComparable<T> {
             for (int i = 0; i < array.Length; i++) {
-                for (int j = 0; j < array.Length; j++) {
-                    if (array[i].CompareTo(array[j]) < 0)
-                        SWAP(array, i, j);
+                for (int j = 0; j < array.Length - 1; j++) {
+                    if (array[j].CompareTo (array[j + 1]) > 0)
+                        SWAP (array, j, j + 1);
                 }
             }
         }
 
-        private static void SWAP<T>(T[] array, int i, int j)
-        {
+        private static void SWAP<T> (T[] array, int i, int j) {
             T temp = array[i];
             array[i] = array[j];
             array[j] = temp;
