9a9aa124d5d7adfaa4a57be6c5025bb2173169a8
Robert <chaolong_chen@hotmail.com>
2018-05-17 01:07:41 +0800

    [List] ArrayList

---
+++ ArrayListExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrays = new ArrayList();
+            arrays.Add(5);
+            arrays.AddRange(new object[] {"Mary", "Joe"});
+            arrays.AddRange(new int[] {1, 2, 3});
+            arrays.Insert(2, "30");
+            foreach (var o in arrays)
+            {
+                System.Console.WriteLine(o);
+            }
+        }
+    }
+}
