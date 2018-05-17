ce97f6637b07aca57ff2732aa113877b6c0e9f1c
Robert <chaolong_chen@hotmail.com>
2018-05-17 23:48:56 +0800

    [Function] To Add SortedList Example.

---
+++ SortedListExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SortedListExample
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public CountryEnum Country { get; set; }
+    }
+
+    public enum CountryEnum
+    {
+        TW,
+        JP,
+        CN,
+        US
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mySortedList = new SortedList<string, Person>();
+            mySortedList.Add ("Robert", new Person() {Name = "Robert", Age = 37, Country = CountryEnum.TW});
+            mySortedList.Add ("Liz", new Person() {Name = "Liz", Age = 29, Country = CountryEnum.TW});
+            mySortedList.Add ("Ann", new Person() {Name = "Ann", Age = 5, Country = CountryEnum.TW});
+            foreach (KeyValuePair<string, Person> person in mySortedList)
+            {
+                System.Console.WriteLine($"{person.Value.Name} {person.Value.Age} Years {person.Value.Country}");
+            }
+        }
+    }
+}
