2741d999102aef140ddf1b16ea4e19ee3fdd89cb
Robert Chen <robert.chen@fitivision.com>
2018-05-22 15:15:57 +0800

    To Add Stack Example

---
+++ StackExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> cstack = new Stack<char>();
+            foreach (var c in "Hello World!")
+            {
+                cstack.Push(c);
+            }
+            
+            while (cstack.Count > 0) {
+                System.Console.Write(cstack.Pop());
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
