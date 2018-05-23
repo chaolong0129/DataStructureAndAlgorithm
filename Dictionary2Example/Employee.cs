3d15d186b6858eab038344c6fbb2bc71b7253ab5
Robert <chaolong_chen@hotmail.com>
2018-05-24 07:59:42 +0800

    [Structure] To Add Dictionary2 Example with instances

---
+++ Dictionary2Example/Employee.cs
@@ -0,0 +1,7 @@
+namespace Dictionary2Example {
+    public class Employee {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
