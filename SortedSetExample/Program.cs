e5e24216ea520e7ad3433932cb937f056fdf0e80
Robert Chen <robert.chen@fitivision.com>
2018-05-28 15:59:32 +0800

    [Structure] To Add SortedSet Example

---
+++ SortedSetExample/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace SortedSetExample {
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+    class Program {
+        static void Main (string[] args) {
+            var names = new List<string> () {
+                "Marcin",
+                "Mary",
+                "James",
+                "Albert",
+                "Lily",
+                "Emily",
+                "marcin",
+                "James",
+                "Jane"
+            };
+
+            var sorted = new SortedSet<string>(
+                names,
+                Comparer<string>.Create((a, b) => 
+                    a.ToLower().CompareTo(b.ToLower()))
+            );
+
+            foreach (var name in sorted)
+            {
+                System.Console.WriteLine(name);
+            }
+        }
+    }
+}
