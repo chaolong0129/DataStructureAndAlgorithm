bd05f9635c73caba465c16703d6f8894028f85f4
Robert Chen <robert.chen@fitivision.com>
2018-06-01 18:09:12 +0800

    To Backup RestSharp Example

---
+++ RestSharpExample/NetworkCommunicationService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using System.Net;
+using System.Collections.Generic;
+
+namespace RestSharpExample
+{
+    class NetworkCommunicationService
+    {
+        private IRestClient Client;
+        private IRestRequest request;
+        private List<string> Stock;
+        public List<string> StockInfo {
+            get { return (Stock.Count !=0 ) ? Stock : null; }
+        }
+
+        public bool IsSuccess { get; private set; }
+
+        public NetworkCommunicationService(string url)
+        {
+            this.Client = new RestClient(url);
+            request = new RestRequest();
+            Stock = new List<string>();
+        }
+
+        private void HttpGetResponse(IRestResponse resp, RestRequestAsyncHandle handle)
+        {
+            if (resp.StatusCode == HttpStatusCode.OK)
+            {
+                Stock.Add(resp.Content);
+                IsSuccess = true;
+            }
+                
+            handle.Abort();
+        }
+
+        public void ExecuteGetAsync()
+        {
+            if (Client == null)
+                return;
+            request.Method = Method.GET;
+            ExecuteGetAsync(request).Wait();
+        }
+
+        private async Task ExecuteGetAsync(IRestRequest req)
+        {
+            if (Client == null)
+                return;
+            await Task.Delay(1);
+            Client.ExecuteAsyncGet(req, HttpGetResponse, "Get");
+        }
+    }
+}
---
+++ RestSharpExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RestSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            var client = new NetworkCommunicationService("https://tw.stock.yahoo.com/q/q?s=2317");
+            while (cnt < 2)
+            {
+                client.ExecuteGetAsync();
+                cnt++;
+            }
+            while (!client.IsSuccess)
+            {
+                if (client.StockInfo == null)
+                    continue;
+                foreach (var stock in client.StockInfo.ToArray())
+                {
+                    Console.WriteLine(stock);
+                }
+                break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
