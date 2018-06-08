20ade7d9d68933ce9e100f106744781b348f9c9d
Robert Chen <robert.chen@fitivision.com>
2018-06-08 10:34:12 +0800

    [Tree] To Add Tree example and add Tree and TreeNode Class

---
+++ Tree/BasicTree/Tree.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicTree
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+    }
+}
